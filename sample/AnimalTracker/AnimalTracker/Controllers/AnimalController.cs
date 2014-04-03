a0273b62bcbf6f825b68f98e4e4acc374334a4f8
Jake Bladt <jake@jakebladt.com>
2014-04-03 07:34:33 -0400

    controller shell

---
+++ sample/AnimalTracker/AnimalTracker/Controllers/AnimalController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalTracker.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public ActionResult All()
+        {
+            return null;
+        }
+
+        public ActionResult Create()
+        {
+            return null;
+        }
+
+        public ActionResult Get(int id)
+        {
+            return null;
+        }
+
+        public ActionResult Update()
+        {
+            return null;
+        }
+
+        public ActionResult Delete()
+        {
+            return null;
+        }
+
+    }
+}
